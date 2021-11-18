bd7a1c77668c2bcdd2f6c1cc1ef56ba099f411f8
Eugene Astafurov <eoastafurov1@gmail.com>
2021-11-18 16:25:07 +0300

    removed unnecessary

--- Lab3/src/main/java/ru/sbt/mipt/Parsing.java
+++
@@ -1,5 +0,0 @@
-package ru.sbt.mipt;
-
-public interface Parsing {
-    Person createPerson(String pathToJson);
-}
--- Lab3/src/main/java/ru/sbt/mipt/Person.java
+++
@@ -1,6 +0,0 @@
-package ru.sbt.mipt;
-
-public interface Person {
-    public String getJson();
-    public ClientType getClientType();
-}
